ce3e7527d02d1393a5e0d326592d63bb6c1808fe
Krystian Górecki <Krystian.gorecki@hycom.pl>
2018-07-19 23:46:09 +0200

    Increased deafult max file size to 4MB.

--- formMain.cs
+++ formMain.cs
@@ -439,7 +439,7 @@ namespace MasterOfWebM
             txtInput.Text = txtOutput.Text = txtSubs.Text = txtLength.Text = txtWidth.Text = "";
             txtTimeStart.Text = "HH:MM:SS";
             txtTimeStart.ForeColor = Color.Silver;
-            txtMaxSize.Text = "3";
+            txtMaxSize.Text = "4";
             txtCrop.Text = "o_w:o_h:x:y";
             txtCrop.ForeColor = Color.Silver;
             txtTitle.Text = "";
